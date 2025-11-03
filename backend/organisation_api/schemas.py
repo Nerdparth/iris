c6717c9acb200ebd4b089a59fe5570a6e4dc9cba
Aprampreet <aprampreetsingh0000@gmail.com>
2025-11-03 12:21:08 +0530

    organisation update delete

--- backend/organisation_api/schemas.py
+++ backend/organisation_api/schemas.py
@@ -8,3 +8,11 @@ class CreateOrganisationSchema(Schema):
 class JoinOrganisationSchema(Schema):
     user_id : str
     organisation_code : str
+
+class UpdateOrganisationSchema(Schema):
+    user_id:str
+    uuid: str
+    new_organisation_name: str = None
+    new_industry: str = None
+
+
--- backend/organisation_api/views.py
+++ backend/organisation_api/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from httpx import post
 from ninja import NinjaAPI
 from django.contrib.auth.models import User
-from .schemas import CreateOrganisationSchema, JoinOrganisationSchema
+from .schemas import CreateOrganisationSchema, JoinOrganisationSchema ,UpdateOrganisationSchema
 from .models import Organisation, OrganisationMember
 import random, string
 
@@ -42,3 +42,47 @@ def join_organisation(request, data: JoinOrganisationSchema):
             return JsonResponse({"error" : "you are already a part of the organisation"} , status=400)
     except Exception as e:
         return JsonResponse({"error" : str(e)}, status=400)
+    
+
+@organisation_api.put('/update-organisation')
+def update_organisation(request, data: UpdateOrganisationSchema):
+    try:
+        if not User.objects.filter(username = data.user_id).exists():
+            return JsonResponse({'error':'you are not a valid user of our Services'}, status=400)
+        else:
+            user = User.objects.get(username = data.user_id)
+        if not Organisation.objects.filter(uuid = data.uuid).exists():
+            return JsonResponse({'error':'incorrect organisation code'}, status=400)
+        else:
+            organisation = Organisation.objects.get(uuid = data.uuid)
+        if not OrganisationMember.objects.filter(member = user, organisation=organisation, is_admin=True).exists():
+            return JsonResponse({'error':'you are not authorised to make these changes'} , status=400)
+        else:
+            if data.new_organisation_name:
+                organisation.organisation_name = data.new_organisation_name
+            if data.new_industry:
+                organisation.industry = data.new_industry
+            organisation.save()
+            return JsonResponse({'message':'organisation details updated successfully'}, status=200)
+    except Exception as e:
+        return JsonResponse({'error':str(e)},status=400)
+    
+
+@organisation_api.delete('/delete-organisation')
+def delete_organisation(request, uuid: str, user_id: str):
+    try:
+        if not User.objects.filter(username=user_id).exists():
+            return JsonResponse({'error':'You are not authorized to make these changes'},status=400)
+        else:
+            user=User.objects.get(username=user_id)
+        if not Organisation.objects.filter(uuid=uuid).exists():
+            return JsonResponse({'error':'incorrect organisation code'},status=400)
+        else:
+            organisation=Organisation.objects.get(uuid=uuid)
+        if not OrganisationMember.objects.filter(member=user,organisation=organisation,is_admin=True).exists():
+            return JsonResponse({'error':'You are not authorized to make these changes'},status=400)
+        else:
+            organisation.delete()
+            return JsonResponse({'message':'organisation deleted successfully'},status=200)
+    except Exception as e:
+        return JsonResponse({'error':str(e)},status=400)
