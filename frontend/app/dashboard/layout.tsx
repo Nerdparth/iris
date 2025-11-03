a88334548f8b1c9bc846d6a3df0a7ba6dd5cb7fd
Dilpreet Singh <dilpreetsinghaulakh@icloud.com>
2025-11-03 15:27:58 +0530

    feat: add Sidebar and Dashboard pages

---
+++ frontend/app/dashboard/layout.tsx
@@ -0,0 +1,30 @@
+import { SignedIn, SignedOut } from "@/components/auth";
+import Sidebar from "./Sidebar";
+import getOrganizationData from "@/api/organization/getOrganizationData";
+// import { SignedIn, SignedOut } from "@clerk/nextjs";
+
+export default async function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const response = await getOrganizationData();
+
+  return (
+    <>
+      <SignedIn>
+        <div className="flex h-[calc(100vh-4rem)] overflow-y-scroll gap-4 font-sans border-t border-gray-200 dark:border-gray-800">
+          <Sidebar bots={response.bots} />
+          <main className="flex-1 p-4">{children}</main>
+        </div>
+      </SignedIn>
+      <SignedOut>
+        <div className="flex h-[calc(100vh-4rem)] items-center justify-center">
+          <p className="text-gray-500">
+            Please sign in to access the dashboard.
+          </p>
+        </div>
+      </SignedOut>
+    </>
+  );
+}
